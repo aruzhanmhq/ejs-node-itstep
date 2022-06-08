0960503ce2d99bd9b02fc8da7cd1900fbd9a5555
Your Name <you@example.com>
2022-06-08 09:08:11 +0600

    laptopsRouter added

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const contactsRouter = require("./routers/contactsRouter");
 const laptopsRouter = require("./routers/laptopsRouter");
 
 const app = express();
@@ -7,7 +6,6 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-app.use('/contacts', contactsRouter);
 app.use('/laptops', laptopsRouter);
 
 app.listen(3030);
--- routers/laptopsRouter.js
+++ routers/laptopsRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const laptopsRouter = express.Router();
 
-let laptopsArr = [
+let laptopsArray = [
     {
         id: 1,
         model: "Lenovo Ideapad",
@@ -12,19 +12,62 @@ let laptopsArr = [
         id: 2,
         model: "Lenovo ThinkPad",
         price: 200000,
-        categoryName: "game"
+        categoryName: "gaming"
     },    
     {
         id: 3,
         model: "Acer Swift",
         price: 180000,
         categoryName: "ultrabook"
-    }
+    },
+    {
+        id: 4,
+        model: "Acer Aspire",
+        price: 230000,
+        categoryName: "office"
+    },
+    {
+        id: 5,
+        model: "Acer Nitro 5",
+        price: 500000,
+        categoryName: "gaming"
+    },    
+    {
+        id: 6,
+        model: "Asus ZenBook",
+        price: 350000,
+        categoryName: "ultrabook"
+    },
+    {
+        id: 7,
+        model: "Acer Extensa",
+        price: 260000,
+        categoryName: "office"
+    },
+    {
+        id: 8,
+        model: "Asus ROG Strix",
+        price: 800000,
+        categoryName: "gaming"
+    },    
+    {
+        id: 9,
+        model: "Asus Vivobook",
+        price: 300000,
+        categoryName: "ultrabook"
+    },
 ]
 
 laptopsRouter.get('/', (req, res) => {
+    res.render('laptops.ejs', {laptopsArray})
+})
+
+laptopsRouter.get('/:categoryName', (req, res) => {
+    let categoryName = req.params.categoryName;
+    let foundLaptops = laptopsArray.filter(item => item.categoryName == categoryName);
+
     res.render('laptops.ejs', {
-        laptopsArray: laptopsArr
+        laptopsArray: foundLaptops
     })
 })
 
